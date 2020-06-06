b0c3e920be8a02db9f47c33990d1cfe5e21bb50f
niupuyue <niupuyue@aliyun.com>
2020-06-06 14:42:58 +0800

    master:添加String工具类

---
+++ bill_base_lib/src/main/java/com/paulniu/bill_base_lib/util/StringUtil.kt
@@ -0,0 +1,24 @@
+package com.paulniu.bill_base_lib.util
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ * @author:Niu Puyue
+ * e-mail:niupuyue@aliyun.com
+ * time:2020/6/6 2:26 PM
+ * desc: 字符串操作相关类
+ */
+
+object StringUtil {
+
+    /**
+     * 清除字符串中的空格
+     */
+    fun formatStringWithoutSpace(value: String?): String {
+        if (value?.isEmpty()!!) return ""
+        val p: Pattern = Pattern.compile("\\s*|\t|\r|\n")
+        val m: Matcher = p.matcher(value)
+        return m.replaceAll("")
+    }
+}
