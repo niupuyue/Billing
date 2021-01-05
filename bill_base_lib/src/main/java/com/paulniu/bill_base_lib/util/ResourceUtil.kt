2be8e39c9f2970d3ee6d20f1405763788cacd3df
niupuyue <niupuyue@aliyun.com>
2021-01-05 15:02:26 +0800

    update: add resourceUtil file to RES TOOLS

--- bill_base_lib/src/main/java/com/paulniu/bill_base_lib/util/ResourceUtil.kt
+++ bill_base_lib/src/main/java/com/paulniu/bill_base_lib/util/ResourceUtil.kt
@@ -1,10 +1,41 @@
 package com.paulniu.bill_base_lib.util
 
+import android.graphics.drawable.Drawable
+import androidx.annotation.*
+import androidx.core.content.ContextCompat
+import com.paulniu.bill_base_lib.base.App
+
 /**
  * @author:Niu Puyue
  * e-mail:niupuyue@aliyun.com
  * time:2021/1/5 12:10 PM
  * desc: 资源相关的工具类
  */
-class ResourceUtil {
+object ResourceUtil {
+
+    @JvmStatic
+    fun getDrawable(@DrawableRes resId: Int): Drawable {
+        return App.getAppContext().resources.getDrawable(resId, null)
+    }
+
+    @JvmStatic
+    fun getColor(@ColorRes id: Int): Int {
+        return ContextCompat.getColor(App.getAppContext(),id)
+    }
+
+    @JvmStatic
+    fun getBoolean(@BoolRes id: Int): Boolean {
+        return App.getAppContext().resources.getBoolean(id)
+    }
+
+    @JvmStatic
+    fun getInteger(@IntegerRes id: Int): Int {
+        return App.getAppContext().resources.getInteger(id)
+    }
+
+    @JvmStatic
+    fun getDimension(@DimenRes id: Int): Float {
+        return App.getAppContext().resources.getDimension(id)
+    }
+
 }
